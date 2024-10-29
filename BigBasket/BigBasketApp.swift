f9ee0ce06941590bfac05f7845171ed9c4ef5a06
SHENU GUPTA <shenu.gupta2009@gmail.com>
2024-10-29 11:22:47 +0530

    Initial Commit

---
+++ BigBasket/BigBasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBasketApp.swift
+//  BigBasket
+//
+//  Created by Shenu Gupta on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigBasket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BigBasket
+//
+//  Created by Shenu Gupta on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
